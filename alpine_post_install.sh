d013ae5d4e8ab7ec0273c290d903dd863093102f
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-06-14 13:22:39 -0500

    Update alpine_post_install.sh

--- alpine_post_install.sh
+++ alpine_post_install.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 
-# Modify repositories to include main and community
-doas sh -c "echo 'https://mirror.math.princeton.edu/pub/alpinelinux/v3.20/main' > /etc/apk/repositories"
-doas sh -c "echo 'https://mirror.math.princeton.edu/pub/alpinelinux/v3.20/community' >> /etc/apk/repositories"
-
-# Update and upgrade the system
-doas apk update
-doas apk upgrade
-
-# Install necessary packages
-packages="sx neovim fastfetch git make fontconfig-dev freetype-dev harfbuzz-dev libxft-dev imlib2-dev dash xsetroot font-jetbrains-mono-nerd gcc g++ libzinerama-dev alsa-lib alsa-utils picom feh rofi font-noto-extra ttf-liberation xf86-input-libinput pciutils ncurses udev firefox"
-doas apk add $packages
-
 # Modify doas.conf for additional permissions
 doas sh -c "echo 'permit persist :wheel' >> /etc/doas.conf"
 doas sh -c "echo 'permit nopass root' >> /etc/doas.conf"
 doas sh -c "echo 'permit persist :wheel cmd reboot' >> /etc/doas.conf"
 doas sh -c "echo 'permit persist :wheel cmd poweroff' >> /etc/doas.conf"
+
+# Install necessary packages
+packages="sx neovim fastfetch make fontconfig-dev freetype-dev harfbuzz-dev libxft-dev imlib2-dev dash xsetroot font-jetbrains-mono-nerd gcc g++ libzinerama-dev alsa-lib alsa-utils picom feh rofi font-noto-extra ttf-liberation xf86-input-libinput pciutils ncurses udev firefox"
+doas apk add $packages
+
 
 # Clone necessary repositories and install
 git clone https://github.com/siduck/chadwm --depth 1 ~/.config/chadwm
